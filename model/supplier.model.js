cf5c6cda8cc8625f930cc732d725e08e042c5519
PriyanshAg-1 <2k22.cse.32203@gmail.com>
2026-01-27 12:16:55 +0530

    Update supplier model with user reference

---
+++ model/supplier.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const SupplierSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    contactInfo: {
+        type: String,
+        required: true
+    },
+    walletAddress: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+module.exports = mongoose.model("Supplier", SupplierSchema);
