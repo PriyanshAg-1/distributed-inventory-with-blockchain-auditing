d96807712323d1ccc25159c4421d3fc713667d97
PriyanshAg-1 <2k22.cse.32203@gmail.com>
2026-01-27 13:34:19 +0530

    Add product model

---
+++ model/product.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
